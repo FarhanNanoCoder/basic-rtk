6dd416f09695c6328c8fe400f29cbe4d149578e4
FarhanNanoCoder <farhan.shariar.094@gmail.com>
2023-07-10 16:22:49 +0600

    feat: setup user module

--- src/modules/user/index.js
+++ src/modules/user/index.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const User = () => {
-  return (
-    <div>User</div>
-  )
-}
+  const [userList, setUserList] = useState([]);
+  const [user, setUser] = useState({ name: "" });
 
-export default User
+  const onCreateUser = () => {
+    console.log(userList, user);
+    let temp = userList;
+    temp.push(user);
+    setUser(null)
+    setUserList(temp);
+  };
+  return (
+    <div className="user-parent">
+      <h2>Users</h2>
+      <input
+        required
+        value={user?.name??""}
+        onChange={(e) => setUser({ name: e.target.value })}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          onCreateUser();
+        }}
+      >
+        Create
+      </button>
+
+      <br />
+      <div>
+        {userList?.length !== 0 ? (
+          userList?.map((item, index) => (
+            <div key={index} style={{ color: "black" }}>
+              {(index + 1).toString() + ". " + item?.name ?? "n/a"}
+            </div>
+          ))
+        ) : (
+          <strong>EMPTY!</strong>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default User;
